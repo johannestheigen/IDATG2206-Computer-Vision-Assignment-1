ea869b5255a4c5ced2c2652758632195e265df50
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2026-02-13 15:24:55 +0100

    Add functionality to process video files!

--- edge_detection.m
+++ edge_detection.m
@@ -1,23 +1,30 @@
+% Cleans the commandline and workspace before processing
 clear all
 clc
 close all
 
-A = imread("tux.png");
-Agray = double(rgb2gray(A));
+video_file = 'Flowers.mp4'; % Variable used to store the file path of the .mp4 file
+v = VideoReader(video_file); % Reads the mp4 file to be processed
 
-K = [-1 -1 -1; 
-     -1  8 -1; 
-     -1  -1 -1];
+while hasFrame(v) % Iterates through every frame contained within the .mp4 file
+    rgbFrame = readFrame(v); % Reads current frame to be processed
+        
+Agray = double(im2gray(rgbFrame)); % Converts the current frame to grayscale
 
-A_padded = padarray(Agray, [1 1], 0, 'both');
+% 3x3 matrix that represents an image kernel used to apply different effects
+K = [0 -1 0; 
+     -1  5 -1; 
+     0  -1 0];
 
-K_flipped = rot90(K,2); 
+A_padded = padarray(Agray, [1 1], 0, 'both'); % Pads the current frame with zeros
 
-[i, j] = size (A_padded);
-[n, m] = size (K);
+K_flipped = rot90(K,2); % Rotates the kernel
 
-C = zeros(size(Agray));
+[i, j] = size (A_padded); % Retrieves the size of the padded matrix
 
+C = zeros(size(Agray)); % Initiates the output matrix with zeros, matching the size of the current grayscale frame. 
+
+% Performs convolution on the current frame 
 for row = 2:(i-1)
     for col = 2:(j-1)
         sub = A_padded(row-1:row+1, col-1:col+1);
@@ -25,4 +32,7 @@ for row = 2:(i-1)
     end
 end
 
-imshow(C, [])
+imshow(C, []) % Displays the current processed frame
+drawnow % Forces MATLAB to update the figure immediately.
+
+end
